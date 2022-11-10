d3563f11322a0075bd1823b76e1a489cbf72a7bc
task11 <task11@naver.com>
2022-11-10 16:45:59 +0900

    fix: keydown 버그 수정

--- src/feature/Search/hooks/useSearchQuery.tsx
+++ src/feature/Search/hooks/useSearchQuery.tsx
@@ -1,12 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { ActionType } from 'typesafe-actions';
 import { getSearchResultsThunk, SearchAction } from '../../../redux/modules';
 
+const ArrowDown = 'ArrowDown' as const;
+const ArrowUp = 'ArrowUp' as const;
+const Escape = 'Escape' as const;
+const Enter = 'Enter' as const;
+
 export default function useSearchQuery(initialState: string) {
   const [searchQuery, setSearchQuery] = useState(initialState);
-
+  const [index, setIndex] = useState<number>(-2);
+  const resultRef = useRef<HTMLDivElement>(null);
   const dispatch = useDispatch();
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (searchQuery.length > 0) {
+      if (event.key === ArrowDown) {
+        setIndex(index + 1);
+        if (resultRef.current?.childElementCount === index + 1) setIndex(0);
+        return;
+      }
+
+      if (event.key === ArrowUp) {
+        setIndex(index - 1);
+        if (index <= 0) {
+          setIndex(0);
+        }
+        return;
+      }
+
+      if (event.key === Escape) {
+        setIndex(index - 1);
+        return;
+      }
+
+      if (event.key === Enter) {
+        setIndex(-1);
+        setSearchQuery(resultRef.current?.childNodes[index]?.textContent || '');
+        return;
+      }
+    }
+  };
 
   const inputSearchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
@@ -47,5 +82,8 @@ export default function useSearchQuery(initialState: string) {
     handleMouseDown,
     inputClear,
     submitSearch,
+    resultRef,
+    handleKeyDown,
+    index,
   };
 }
